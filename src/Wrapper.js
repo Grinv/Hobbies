3115f59829c21ff872060a72c0f23dbfb2847a02
Konstantin Ostrovskiy <kostrovskiy-contractor@evernote.com>
2018-11-16 19:59:36 +0300

    Initial commit

---
+++ src/Wrapper.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Wrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tagInputValue: '',
+    };
+  }
+  render() {
+    return (
+      <div>123</div>
+    );
+  }
+}
---
+++ src/entry.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Wrapper from './Wrapper';
+
+window.onload = () => ReactDOM.render(React.createElement(Wrapper), document.getElementById('root'));
